9275f89d5be4b94e976e02fc2e9376eaf1718240
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-04 11:03:21 -0500

    simplify general script for GWAS format and LDSC heritability

--- scripts/ldsc_genetic_heritability_correlation/format_munge_gwas_heritability_ldsc.sh
+++ scripts/ldsc_genetic_heritability_correlation/format_munge_gwas_heritability_ldsc.sh
@@ -15,11 +15,10 @@ path_source_file=${2} # full path to source file with GWAS summary statistics
 path_genetic_reference=${3} # full path to parent directory with genetic reference files for LDSC
 path_gwas=${4} # full path to parent directory for formatted GWAS summary statistics
 path_heritability=${5} # full path to parent directory for LDSC heritability estimation
-path_genetic_correlation=${6} # full path to parent directory for LDSC genetic correlation
-path_script_gwas_format=${7} # full path to script to use to organize format of GWAS summary statistics for phenotype
-path_promiscuity_scripts=${8} # complete path to directory of scripts for z-score standardization
-path_ldsc=${9} # path to LDSC
-report=${10} # whether to print reports
+path_script_gwas_format=${6} # full path to script to use to organize format of GWAS summary statistics for phenotype
+path_promiscuity_scripts=${7} # complete path to directory of scripts for z-score standardization
+path_ldsc=${8} # path to LDSC
+report=${9} # whether to print reports
 
 ################################################################################
 # Organize variables.
@@ -32,10 +31,7 @@ path_frequencies="$path_genetic_reference/frequencies"
 
 path_study_gwas="${path_gwas}/${study}"
 path_study_heritability="${path_heritability}/${study}"
-path_study_genetic_correlation="${path_genetic_correlation}/${study}"
-mkdir -p $path_study_gwas
-mkdir -p $path_study_heritability
-mkdir -p $path_study_genetic_correlation
+
 path_gwas_collection="${path_study_gwas}/gwas_collection.txt"
 path_gwas_format="${path_study_gwas}/gwas_format.txt"
 path_gwas_format_compress="${path_gwas_format}.gz"
@@ -57,6 +53,14 @@ if [[ "$report" == "true" ]]; then
   echo "path to original source file: " $path_source_file
   echo "path to new file: " $path_gwas_format
   echo "----------"
+fi
+
+# Initialize directories.
+if [ ! -d $path_study_gwas ]; then
+    mkdir -p $path_study_gwas
+fi
+if [ ! -d $path_study_heritability ]; then
+    mkdir -p $path_study_heritability
 fi
 
 # Organize information in format for LDSC.
@@ -96,5 +100,5 @@ fi
 # Remove temporary files.
 rm $path_gwas_collection
 rm $path_gwas_format
-rm $path_gwas_munge_suffix
-rm $path_gwas_munge_log
+#rm $path_gwas_munge_suffix
+#rm $path_gwas_munge_log
