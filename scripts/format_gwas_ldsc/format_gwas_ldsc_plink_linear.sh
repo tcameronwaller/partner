bfdff20d40244afcc65a62d9e450e667051a16b9
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-06 12:57:53 -0500

    new format gwas script for plink linear

---
+++ scripts/format_gwas_ldsc/format_gwas_ldsc_plink_linear.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+# "Organize GWAS summary statistics."
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+################################################################################
+# Organize variables.
+study=${1} # unique identifier of the current GWAS study
+path_source_file=${2} # full path to source file with GWAS summary statistics
+path_gwas_collection=${3} # full path to temporary file for collection of GWAS summary statistics
+path_gwas_format=${4} # full path to file for formatted GWAS summary statistics
+path_gwas_format_compress=${5} # full path to file for formatted GWAS summary statistics after compression
+path_promiscuity_scripts=${6} # complete path to directory of scripts for z-score standardization
+report=${7} # whether to print reports
+
+#path_calculate_z_score="$path_promiscuity_scripts/calculate_z_score_column_4_of_5.sh"
+path_calculate_z_score="$path_promiscuity_scripts/calculate_z_score_column_5_of_6.sh"
+
+###########################################################################
+# Execute procedure.
+
+# Report.
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "study: " $study
+  echo "path to original file: " $path_source_file
+  echo "path to new file: " $path_gwas_format
+fi
+
+# Format of GWAS summary statistics for LDSC.
+# https://github.com/bulik/ldsc/wiki/Heritability-and-Genetic-Correlation#reformatting-summary-statistics
+# Format of GWAS reports by PLINK2 for linear regression (".glm.linear").
+# https://www.cog-genomics.org/plink/2.0/formats
+# PLINK2 report format is similar for logistic regression (".glm.logistic").
+# Logistic report has "OR" in place of "BETA", but positions are the same.
+# description: ............................ LDSC column ........... source column .......... position
+# variant identifier (RS ID): .............  "SNP" ................  "ID" .................. 3
+# alternate allele (effect allele): .......  "A1" .................  "A1" .................. 6
+# reference allele (non-effect allele): ...  "A2" .................  "REF" ................. 4
+# sample size: ............................  "N" ..................  "OBS_CT" .............. 8
+# effect (coefficient or odds ratio): .....  "BETA" or "OR" .......  "BETA" ................ 9
+# probability (p-value): ..................  "P" ..................  "P" ................... 12
+
+# Remove any previous versions of temporary files.
+rm $path_gwas_collection
+rm $path_gwas_format
+
+# Organize information from linear GWAS.
+echo "SNP A1 A2 N BETA P" > $path_gwas_collection
+zcat $path_source_file | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {print $3, toupper($6), toupper($4), $8, $9, $12}' >> $path_gwas_collection
+# Calculate Z-score standardization of Beta coefficients.
+/usr/bin/bash $path_calculate_z_score \
+5 \
+$path_gwas_collection \
+$path_gwas_format \
+$report
+
+# Compress file format.
+gzip -cvf $path_gwas_format > $path_gwas_format_compress
+
+# Report.
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "before standardization:"
+  head -10 $path_gwas_collection
+  echo "after standardization:"
+  head -10 $path_gwas_format
+  echo "----------"
+fi
