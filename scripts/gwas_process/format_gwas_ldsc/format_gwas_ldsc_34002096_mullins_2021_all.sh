77042cfe2164cfb618d19c1d8cbde520e719ede9
T. Cameron Waller <tcameronwaller@gmail.com>
2021-09-03 21:15:59 -0500

    preliminary updates to GWAS format scripts

--- scripts/gwas_process/format_gwas_ldsc/format_gwas_ldsc_34002096_mullins_2021_all.sh
+++ scripts/gwas_process/format_gwas_ldsc/format_gwas_ldsc_34002096_mullins_2021_all.sh
@@ -15,25 +15,21 @@
 # note: on 5 April 2021, TCW confirmed that table body begins at "awk NR > 73" in all three files
 # note: on 5 April 2021, TCW confirmed that table columns have the same order in all three files
 
-###########################################################################
-###########################################################################
-###########################################################################
+# zcat ./pgc-bip2021-all.vcf.tsv.gz | head -n 73
 
-################################################################################
-# Organize arguments.
-study=${1} # unique identifier of the current GWAS study
-path_source_file=${2} # full path to source file with GWAS summary statistics
-path_gwas_collection=${3} # full path to temporary file for collection of GWAS summary statistics
-path_gwas_format=${4} # full path to file for formatted GWAS summary statistics
-path_gwas_format_compress=${5} # full path to file for formatted GWAS summary statistics after compression
-path_promiscuity_scripts=${6} # complete path to directory of scripts for z-score standardization
-report=${7} # whether to print reports
+###########################################################################
+###########################################################################
+###########################################################################
 
 ################################################################################
 # Organize variables.
-
-#path_calculate_z_score="$path_promiscuity_scripts/calculate_z_score_column_4_of_5.sh"
-path_calculate_z_score="$path_promiscuity_scripts/calculate_z_score_column_5_of_6.sh"
+path_gwas_source=${1} # full path to source file with GWAS summary statistics, with gzip compression
+path_gwas_constraint=${2} # full path to temporary file for constraint of GWAS summary statistics to bounds
+path_gwas_format=${3} # full path to file for formatted GWAS summary statistics
+path_gwas_standard=${4} # full path to file for GWAS summary statistics with standard z-scores
+path_gwas_format_compress=${5} # full path to file for formatted GWAS summary statistics after compression
+path_script_calculate_z_score=${6} # full path to directory of scripts for z-score standardization
+report=${7} # whether to print reports
 
 ###########################################################################
 # Execute procedure.
@@ -41,13 +37,49 @@ path_calculate_z_score="$path_promiscuity_scripts/calculate_z_score_column_5_of_
 # Report.
 if [[ "$report" == "true" ]]; then
   echo "----------"
-  echo "study: " $study
-  echo "path to original file: " $path_source_file
-  echo "path to new file: " $path_gwas_format
+  echo "path to source file: " $path_gwas_source
+  echo "path to target file: " $path_gwas_format_compress
 fi
 
+# Here are some useful regular expressions to evaluate values in "awk".
+# ( $12 ~ /^[0-9]+$/ ); ( $12 ~ /^[[:alpha:]]+$/ ); ( $12 ~ /^[[:punct:]]+$/ )
+
+# Remove any previous versions of temporary files.
+rm $path_gwas_constraint
+rm $path_gwas_format
+rm $path_gwas_standard
+rm $path_gwas_format_compress
+
+##########
+# Constrain GWAS probability values within bounds for LDSC.
+# Probability values in column "P" from PLINK2 have values of "NA" for missing
+# or floating point values between zero and one.
+# LDSC uses 64-bit floating point precision (double precision) to represent
+# values from +/- 2.23E-308 to +/- 1.80E308 (https://github.com/bulik/ldsc/issues/144).
+# Constrain probability values from 1.0E-305 to 1.0.
+zcat $path_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR == 73' > $path_gwas_constraint
+zcat $path_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR > 73 {
+  if ( NF != 16)
+    # Skip any rows with incorrect count of column fields.
+    next
+  else if ( ( $8 != "NA" ) && ( ($8 + 0) < 1.0E-305 ) )
+    # Constrain probability value.
+    print $1, $2, $3, $4, $5, $6, $7, ( 1.0E-305 ), $9, $10, $11, $12, $13, $14, $15, $16
+  else if ( ( $8 != "NA" ) && ( ($8 + 0) > 1.0 ) )
+    # Constrain probability value.
+    print $1, $2, $3, $4, $5, $6, $7, ( 1.0 ), $9, $10, $11, $12, $13, $14, $15, $16
+  else
+    print $0
+  }' >> $path_gwas_constraint
+
+# TODO: TCW 3 September 2021
+# TODO: I still need to edit below here for all 3 scripts for "_mullins_2021"
+
+
+##########
 # Format of GWAS summary statistics for LDSC.
 # https://github.com/bulik/ldsc/wiki/Heritability-and-Genetic-Correlation#reformatting-summary-statistics
+
 # description: ............................ LDSC column ........... source column .......... position
 # variant identifier (RS ID): .............  "SNP" ................  "ID" ..................  3
 # alternate allele (effect allele): .......  "A1" .................  "A1" ..................  4
@@ -62,7 +94,7 @@ rm $path_gwas_format
 
 # Organize information from linear GWAS.
 echo "SNP A1 A2 N BETA P" > $path_gwas_collection
-zcat $path_source_file | awk 'BEGIN { FS=" "; OFS=" " } NR > 73 {print $3, toupper($4), toupper($5), ($14 + $15), $6, $8}' >> $path_gwas_collection
+zcat $path_source_file | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {print $3, toupper($4), toupper($5), ($14 + $15), $6, $8}' >> $path_gwas_collection
 # Calculate Z-score standardization of Beta coefficients.
 /usr/bin/bash $path_calculate_z_score \
 5 \
