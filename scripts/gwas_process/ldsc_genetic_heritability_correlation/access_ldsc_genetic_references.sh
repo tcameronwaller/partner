121bb1569a5660fa454567e470479e2b21302bbf
T. Cameron Waller <tcameronwaller@gmail.com>
2021-10-25 13:41:30 -0500

    new paths for downloads for LDSC reference

--- scripts/gwas_process/ldsc_genetic_heritability_correlation/access_ldsc_genetic_references.sh
+++ scripts/gwas_process/ldsc_genetic_heritability_correlation/access_ldsc_genetic_references.sh
@@ -42,34 +42,42 @@ fi
 ###########################################################################
 # Access references for LDSC.
 
+# View repository files.
+# https://alkesgroup.broadinstitute.org/LDSCORE/
+
 cd $path_genetic_reference
 
 # Definitions of Simple Nucleotide Variant alleles.
-wget https://data.broadinstitute.org/alkesgroup/LDSCORE/w_hm3.snplist.bz2
+#wget https://alkesgroup.broadinstitute.org/LDSCORE/w_hm3.snplist.bz2
+wget https://storage.googleapis.com/broad-alkesgroup-public/LDSCORE/w_hm3.snplist.bz2
 bunzip2 "$path_genetic_reference/w_hm3.snplist.bz2"
 mv "$path_genetic_reference/w_hm3.snplist" "$path_alleles/w_hm3.snplist"
 # w_hm3.snplist
 
 # Linkage disequilibrium scores for European population.
 # For simple heritability estimation.
-wget https://data.broadinstitute.org/alkesgroup/LDSCORE/eur_w_ld_chr.tar.bz2
+#wget https://data.broadinstitute.org/alkesgroup/LDSCORE/eur_w_ld_chr.tar.bz2
+wget https://storage.googleapis.com/broad-alkesgroup-public/LDSCORE/eur_w_ld_chr.tar.bz2
 tar -xjvf eur_w_ld_chr.tar.bz2 -C $path_disequilibrium
 # dock/access/disequilibrium/eur_w_ld_chr/*
 
 # Baseline model linkage disequilibrium scores.
 # For partitioned heritability estimation by stratified LD score regression.
-wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_baselineLD_v2.2_ldscores.tgz
+#wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_baselineLD_v2.2_ldscores.tgz
+wget https://storage.googleapis.com/broad-alkesgroup-public/LDSCORE/1000G_Phase3_baselineLD_v2.2_ldscores.tgz
 tar -xzvf 1000G_Phase3_baselineLD_v2.2_ldscores.tgz -C $path_baseline
 # dock/access/baseline/baselineLD.*
 
 # Weights.
 # For partitioned heritability estimation by stratified LD score regression.
-wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_weights_hm3_no_MHC.tgz
+#wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_weights_hm3_no_MHC.tgz
+wget https://storage.googleapis.com/broad-alkesgroup-public/LDSCORE/1000G_Phase3_weights_hm3_no_MHC.tgz
 tar -xzvf 1000G_Phase3_weights_hm3_no_MHC.tgz -C $path_weights
 # dock/access/weights/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC.*
 
 # Frequencies.
 # For partitioned heritability estimation by stratified LD score regression.
-wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_frq.tgz
+#wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_frq.tgz
+wget https://storage.googleapis.com/broad-alkesgroup-public/LDSCORE/1000G_Phase3_frq.tgz
 tar -xzvf 1000G_Phase3_frq.tgz -C $path_frequencies
 # dock/access/frequencies/1000G_Phase3_frq/1000G.EUR.QC.*
