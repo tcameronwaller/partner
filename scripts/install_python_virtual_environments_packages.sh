5dfb8f5fd175603f25bcde757e742d20bb58e621
T. Cameron Waller <tcameronwaller@gmail.com>
2021-12-09 15:54:26 -0600

    some work on script for Python virtual environments

--- scripts/install_python_virtual_environments_packages.sh
+++ scripts/install_python_virtual_environments_packages.sh
@@ -15,6 +15,8 @@ path_python_2718="${path_python}/python_2.7.18"
 path_environment_main="${path_tools}/python/environments/main"
 path_environment_ldsc="${path_tools}/python/environments/ldsc"
 
+path_environment_prs_cs="${path_tools}/python/environments/prs_cs"
+
 # Initialize directories.
 mkdir -p $path_python
 mkdir -p $path_python_396
@@ -148,6 +150,40 @@ which python3
 
 # Delete virtual environment.
 # rm -rf [virtual environment path and name] # remove virtual environment
+
+################################################################################
+# Python3 virtual environment
+# Python standard module "venv" is the preferred Virtual Environment manager for
+# Python version 3.3 or higher.
+# https://docs.python.org/3/tutorial/venv.html
+# [path to specific python3 installation] -m venv [path to new virtual environment and its name to create]
+# source [virtual environment name]/bin/activate
+# deactivate
+
+"${path_python_396}/bin/python3" -m venv --help # TCW, __ December 2021
+
+"${path_python_396}/bin/python3" -m venv $path_environment_prs_cs # TCW, __ December 2021
+
+# Activate virtual environment.
+# Unable to install with "--user" flag within virtual environment.
+# ERROR: Can not perform a '--user' install. User site-packages are not visible in this virtualenv.
+source "${path_environment_prs_cs}/bin/activate"
+which python3 # "${path_environment_main}/bin/python3" TCW, 28 July 2021
+# Pip installation within virtual environment should not require "sudo" permissions.
+python3 -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip setuptools
+python3 -m pip --version # "pip 21.1.3 from ${path_environment_main}/lib/python3.9/site-packages/pip (python 3.9)" TCW, __ December 2021
+python3 -m pip install --upgrade pip
+python3 -m pip --version # "pip 21.2.1 from ${path_environment_main}/lib/python3.9/site-packages/pip (python 3.9)" TCW, __ December 2021
+python3 -m pip install --upgrade numpy # "numpy-1.21.1" TCW, __ December 2021
+python3 -m pip install --upgrade scipy # "scipy-1.7.0" TCW, __ December 2021
+python3 -m pip install --upgrade h5py # "___" TCW, __ December 2021
+python3 -m pip install --upgrade testresources # "pbr-5.6.0 testresources-2.0.1" TCW, __ December 2021
+deactivate
+which python3
+
+# Delete virtual environment.
+# rm -rf [virtual environment path and name] # remove virtual environment
+
 
 ################################################################################
 # Python2 virtual environment
