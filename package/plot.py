7d583dc0f978b0478eb6cb4db09e8e81aa80d72e
T. Cameron Waller <tcameronwaller@gmail.com>
2022-04-20 11:24:19 -0500

    plot tweaks

--- package/plot.py
+++ package/plot.py
@@ -42,6 +42,7 @@ import pickle
 import itertools
 import statistics
 import copy
+import math
 
 # Relevant
 import pandas
@@ -346,7 +347,7 @@ def define_color_properties():
     # Black.
     black = (0.0, 0.0, 0.0, 1.0)
     # Gray.
-    gray = (0.7, 0.7, 0.7, 1.0)
+    gray = (0.6, 0.6, 0.6, 1.0)
     # White.
     white = (1.0, 1.0, 1.0, 1.0)
     white_faint = (1.0, 1.0, 1.0, 0.75)
