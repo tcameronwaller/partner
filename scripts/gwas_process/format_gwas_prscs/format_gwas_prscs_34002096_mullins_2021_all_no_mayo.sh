4f5648d4e9dc998048d34cd69960b6a31582a760
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-24 17:10:52 -0500

    script to format Bipolar Disorder GWAS for PRS-CS

--- scripts/gwas_process/format_gwas_prscs/format_gwas_prscs_34002096_mullins_2021_all_no_mayo.sh
+++ scripts/gwas_process/format_gwas_prscs/format_gwas_prscs_34002096_mullins_2021_all_no_mayo.sh
@@ -11,14 +11,13 @@
 # phenotype: bipolar disorder types I and II
 # Human genome version: GRCh37; article did specify
 # variant identifier (rsID) version: ???
-# file: "pgc-bip2021-all.vcf.tsv.gz", "pgc-bip2021-BDI.vcf.tsv.gz", "pgc-bip2021-BDII.vcf.tsv.gz"
-# note: on 5 April 2021, TCW confirmed that table body begins at "awk NR > 73" in all three files
-# note: on 5 April 2021, TCW confirmed that table columns have the same order in all three files
-
-# zcat ./pgc-bip2021-all.vcf.tsv.gz | head -n 73
+# file: "daner_bip_pgc3_nm_nomay1.gz"
+# note:
+# Authors performed GWAS with samples from all cohorts except for Mayo Clinic.
+# This was a leave-one-out analysis.
 
 # review:
-# TCW 7 December 2021
+# TCW 24 May 2022
 
 ###########################################################################
 ###########################################################################
@@ -29,11 +28,8 @@
 path_gwas_source=${1} # full path to source file with GWAS summary statistics, with gzip compression
 path_gwas_constraint=${2} # full path to temporary file for constraint of GWAS summary statistics to bounds
 path_gwas_format=${3} # full path to file for formatted GWAS summary statistics
-path_gwas_standard=${4} # full path to file for GWAS summary statistics with standard z-scores
-path_gwas_format_compress=${5} # full path to file for formatted GWAS summary statistics after compression
-path_script_calculate_z_score=${6} # full path to directory of scripts for z-score standardization
-response_standard_scale=${7} # whether to convert reponse (effect, coefficient) to z-score standard scale ("true" or "false")
-report=${8} # whether to print reports
+path_gwas_format_compress=${4} # full path to file for formatted GWAS summary statistics after compression
+report=${5} # whether to print reports
 
 ###########################################################################
 # Execute procedure.
@@ -51,7 +47,6 @@ fi
 # Remove any previous versions of temporary files.
 rm $path_gwas_constraint
 rm $path_gwas_format
-rm $path_gwas_standard
 rm $path_gwas_format_compress
 
 ##########
@@ -61,74 +56,79 @@ rm $path_gwas_format_compress
 # LDSC uses 64-bit floating point precision (double precision) to represent
 # values from +/- 2.23E-308 to +/- 1.80E308 (https://github.com/bulik/ldsc/issues/144).
 # Constrain probability values from 1.0E-305 to 1.0.
-zcat $path_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR == 73' > $path_gwas_constraint
-zcat $path_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR > 73 {
-  if ( NF != 16)
+zcat $path_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR == 1' > $path_gwas_constraint
+zcat $path_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {
+  if ( NF != 19)
     # Skip any rows with incorrect count of column fields.
     next
-  else if ( ( $8 != "NA" ) && ( ($8 + 0) < 1.0E-305 ) )
+  else if ( ( $9 == "NA" ) || ( $11 == "NA" ) )
+    # Records for SNPs with missing values in response and probability do not contribute.
+    # Skip these.
+    next
+  else if ( ( $11 != "NA" ) && ( ($11 + 0) < 1.0E-305 ) )
     # Constrain probability value.
-    print $1, $2, $3, $4, $5, $6, $7, ( 1.0E-305 ), $9, $10, $11, $12, $13, $14, $15, $16
-  else if ( ( $8 != "NA" ) && ( ($8 + 0) > 1.0 ) )
+    print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, ( 1.0E-305 ), $12, $13, $14, $15, $16, $17, $18, $19
+  else if ( ( $11 != "NA" ) && ( ($11 + 0) > 1.0 ) )
     # Constrain probability value.
-    print $1, $2, $3, $4, $5, $6, $7, ( 1.0 ), $9, $10, $11, $12, $13, $14, $15, $16
+    print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, ( 1.0 ), $12, $13, $14, $15, $16, $17, $18, $19
   else
     print $0
   }' >> $path_gwas_constraint
 
-# TODO: TCW 3 September 2021
-# TODO: I still need to edit below here for all 3 scripts for "_mullins_2021"
-
-
 ##########
-# Format of GWAS summary statistics for LDSC.
-# https://github.com/bulik/ldsc/wiki/Heritability-and-Genetic-Correlation#reformatting-summary-statistics
+# Format of GWAS summary statistics for PRS-CS.
+# https://github.com/getian107/PRScs
 
-# description: ............................ LDSC column ........... source column .......... position
-# variant identifier (RS ID): .............  "SNP" ................  "ID" ..................  3
-# alternate allele (effect allele): .......  "A1" .................  "A1" ..................  4
-# reference allele (non-effect allele): ...  "A2" .................  "A2" ..................  5
-# sample size: ............................  "N" ..................  "NCAS" + "NCON" .......  14 + 15
-# effect (coefficient or odds ratio): .....  "BETA" or "OR" .......  "BETA" ................  6
-# probability (p-value): ..................  "P" ..................  "PVAL" ................  8
+# Format of GWAS reports by PLINK2 for linear regression (".glm.linear").
+# https://www.cog-genomics.org/plink/2.0/formats
+# PLINK2 report format is similar for logistic regression (".glm.logistic").
+# Logistic report has "OR" in place of "BETA", but positions are the same.
+
+# PLINK2's format documentation describes column "REF" as "Reference allele" and
+# column "ALT" as "All alternate alleles, comma-separated". Column "REF" and
+# column "ALT" are always different (TCW, 7 July 2021).
+# PLINK2's format documentation also describes column "A1" as "Counted allele in
+# regression". Column "A1" always matches either column "REF" or column "ALT"
+# (TCW, 7 July 2021).
+
+# The designations of "reference" and "alternate" alleles are irrelevant to
+# downstream analyses on GWAS summary statistics (such as LDSC).
+# What matters is which allele PLINK2 counted as the "effect" allele in GWAS
+# regression and which allele was the other allele.
+
+# Column "A1" is the "effect" allele that PLINK2 counted in the regression,
+# corresponding to the coefficient (beta).
+# Determine the "non-effect" or "other" allele from either the "reference" or
+# "alternate" allele, whichever differs from the "effect" allele.
+
+# description: ............................ PRS-CS column ......... PLINK column .......... position
+# variant identifier (RS ID): .............  "SNP" ................  "ID" .................. 2
+# alternate allele (effect allele): .......  "A1" .................  "A1" .................. 4
+# reference allele (non-effect allele): ...  "A2" .................  "REF" or "ALT" ........ 5
+# effect (coefficient or odds ratio): .....  "BETA" or "OR" .......  "OR" .................. 9
+# probability (p-value): ..................  "P" ..................  "P" ................... 11
 
 # Organize information from linear GWAS.
-if [[ "$response_standard_scale" == "true" ]]; then
-  echo "SNP A1 A2 N Z P" > $path_gwas_format
-else
-  echo "SNP A1 A2 N BETA P" > $path_gwas_format
-fi
-cat $path_gwas_constraint | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {print $3, toupper($4), toupper($5), ($14 + $15), $6, $8}' >> $path_gwas_format
-
-##########
-
-# Calculate Z-score standardization of Beta coefficients.
-if [[ "$response_standard_scale" == "true" ]]; then
-  /usr/bin/bash $path_script_calculate_z_score \
-  5 \
-  $path_gwas_format \
-  $path_gwas_standard \
-  $report
-else
-  cp $path_gwas_format $path_gwas_standard
-fi
+# Select relevant columns and place them in the correct order.
+# Keep original 'odds ratio' from logistic regression.
+echo "SNP A1 A2 OR P" > $path_gwas_format
+cat $path_gwas_constraint | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {
+  print $2, toupper($4), toupper($5), $9, $11
+}' >> $path_gwas_format
 
 # Compress file format.
-gzip -cvf $path_gwas_standard > $path_gwas_format_compress
+gzip -cvf $path_gwas_format > $path_gwas_format_compress
 
 # Report.
 if [[ "$report" == "true" ]]; then
   echo "----------"
   echo "after bound constraints:"
   head -10 $path_gwas_constraint
-  echo "before standardization:"
+  echo "after format:"
   head -10 $path_gwas_format
-  echo "after standardization:"
-  head -10 $path_gwas_standard
   echo "----------"
 fi
 
 # Remove temporary files.
 rm $path_gwas_constraint
 rm $path_gwas_format
-rm $path_gwas_standard
--- scripts/utility/prscs_polygenic_score/drive_format_gwas_prscs.sh
+++ scripts/utility/prscs_polygenic_score/drive_format_gwas_prscs.sh
@@ -11,25 +11,19 @@
 
 ################################################################################
 # Organize arguments.
-path_gwas_concatenation_compress=${1} # full path to file for source GWAS summary statistics
-path_gwas_target_parent=${2} # full path to parent directory for target GWAS summary statistics
-path_promiscuity_scripts=${3} # full path to directory of general scripts
-path_script_format_gwas=${4} # full path to script for format adjustment
-report=${5} # whether to print reports
+path_gwas_source=${1} # full path to file for source GWAS summary statistics
+path_gwas_product_container=${2} # full path to directory for product GWAS summary statistics
+path_script_format_gwas=${3} # full path to script for format adjustment
+report=${4} # whether to print reports
 
 ################################################################################
 # Paths.
-path_gwas_constraint="${path_gwas_target_parent}/gwas_constraint.txt"
-path_gwas_format="${path_gwas_target_parent}/gwas_format.txt"
-path_gwas_format_compress="${path_gwas_target_parent}/gwas_format.txt.gz"
-
-################################################################################
-# Scripts.
-path_scripts_gwas_process="${path_promiscuity_scripts}/gwas_process"
+path_gwas_constraint="${path_gwas_product_container}/gwas_constraint.txt"
+path_gwas_format="${path_gwas_product_container}/gwas_format.txt"
+path_gwas_format_compress="${path_gwas_product_container}/gwas_format.txt.gz"
 
 ################################################################################
 # Format adaptation.
-path_gwas_source=$path_gwas_concatenation_compress
 /usr/bin/bash "$path_script_format_gwas" \
 $path_gwas_source \
 $path_gwas_constraint \
