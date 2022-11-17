7f5e8c451da762db0c1142224004fe15c66817ce
T. Cameron Waller <tcameronwaller@gmail.com>
2022-11-16 18:12:34 -0600

    correction to format translation columns

--- scripts/gwas_process/format_gwas_team/translate_gwas_pgc_body_bipolar.sh
+++ scripts/gwas_process/format_gwas_team/translate_gwas_pgc_body_bipolar.sh
@@ -59,7 +59,7 @@ rm $path_file_product
 
 echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_temporary_format
 zcat $path_file_source | awk 'BEGIN { FS=","; OFS=" " } NR > 1 {
-  split($1,a,"_"); print $1, a[1], a[2], toupper($2), toupper($3), "NA", $8, $9, $10, (3717), "NA", (1), "NA", "NA"
+  split($1,a,"_"); print $1, a[1], a[2], toupper($2), toupper($3), "NA", $5, $6, $7, (3717), "NA", (1), "NA", "NA"
 }' >> $path_file_temporary_format
 
 # Compress file format.
@@ -82,49 +82,6 @@ fi
 
 # Remove temporary, intermediate files.
 rm -r $path_directory_product_temporary
-
-
-
-##########
-# Notes on format of GWAS summary statistics from PLINK2.
-
-# Format of GWAS reports by PLINK2 for linear regression (".glm.linear").
-# https://www.cog-genomics.org/plink/2.0/formats
-# PLINK2 report format is similar for logistic regression (".glm.logistic").
-# Logistic report has "OR" in place of "BETA", but positions are the same.
-
-# PLINK2's format documentation describes column "REF" as "Reference allele" and
-# column "ALT" as "All alternate alleles, comma-separated". Column "REF" and
-# column "ALT" are always different (TCW, 7 July 2021).
-# PLINK2's format documentation also describes column "A1" as "Counted allele in
-# regression". Column "A1" always matches either column "REF" or column "ALT"
-# (TCW, 7 July 2021).
-
-# The designations of "reference" and "alternate" alleles are irrelevant to
-# downstream analyses on GWAS summary statistics (such as LDSC).
-# What matters is which allele PLINK2 counted as the "effect" allele in GWAS
-# regression and which allele was the other allele.
-
-# Column "A1" is the "effect" allele that PLINK2 counted in the regression,
-# corresponding to the coefficient (beta).
-# Determine the "non-effect" or "other" allele from either the "reference" or
-# "alternate" allele, whichever differs from the "effect" allele.
-
-# description: ............................ Team column ........... PLINK column .......... position
-# variant identifier (RS ID): .............  "SNP" ................  "ID" ..................  3
-# chromosome: .............................  "CHR" ................  "CHROM" ...............  1
-# base pair coordinate: ...................  "BP" .................  "POS" .................  2
-# alternate allele (effect allele): .......  "A1" .................  "A1" ..................  6
-# reference allele (non-effect allele): ...  "A2" .................  "REF" or "ALT" ........  4 or 5
-# alternate allele frequency: .............  "A1AF" ...............  "A1_FREQ" .............  7
-# effect (coefficient): ...................  "BETA" ...............  "BETA" ................ 10
-# effect standard error: ..................  "SE" .................  "SE" .................. 11
-# probability (p-value): ..................  "P" ..................  "P" ................... 13
-# count total samples: ....................  "N" ..................  "OBS_CT" ..............  9
-# probability (p-value): ..................  "Z" ..................  "NA" .................. (BETA / SE) # division by zero
-# imputation quality score: ...............  "INFO" ...............  "NA" .................. NA
-# count case samples: .....................  "NCASE" ..............  "NA" .................. NA
-# count control samples: ..................  "NCONT" ..............  "NA" .................. NA
 
 
 
--- scripts/gwas_process/format_gwas_team/translate_gwas_r_metafor_body_bipolar.sh
+++ scripts/gwas_process/format_gwas_team/translate_gwas_r_metafor_body_bipolar.sh
@@ -83,47 +83,4 @@ rm -r $path_directory_product_temporary
 
 
 
-##########
-# Notes on format of GWAS summary statistics from PLINK2.
-
-# Format of GWAS reports by PLINK2 for linear regression (".glm.linear").
-# https://www.cog-genomics.org/plink/2.0/formats
-# PLINK2 report format is similar for logistic regression (".glm.logistic").
-# Logistic report has "OR" in place of "BETA", but positions are the same.
-
-# PLINK2's format documentation describes column "REF" as "Reference allele" and
-# column "ALT" as "All alternate alleles, comma-separated". Column "REF" and
-# column "ALT" are always different (TCW, 7 July 2021).
-# PLINK2's format documentation also describes column "A1" as "Counted allele in
-# regression". Column "A1" always matches either column "REF" or column "ALT"
-# (TCW, 7 July 2021).
-
-# The designations of "reference" and "alternate" alleles are irrelevant to
-# downstream analyses on GWAS summary statistics (such as LDSC).
-# What matters is which allele PLINK2 counted as the "effect" allele in GWAS
-# regression and which allele was the other allele.
-
-# Column "A1" is the "effect" allele that PLINK2 counted in the regression,
-# corresponding to the coefficient (beta).
-# Determine the "non-effect" or "other" allele from either the "reference" or
-# "alternate" allele, whichever differs from the "effect" allele.
-
-# description: ............................ Team column ........... PLINK column .......... position
-# variant identifier (RS ID): .............  "SNP" ................  "ID" ..................  3
-# chromosome: .............................  "CHR" ................  "CHROM" ...............  1
-# base pair coordinate: ...................  "BP" .................  "POS" .................  2
-# alternate allele (effect allele): .......  "A1" .................  "A1" ..................  6
-# reference allele (non-effect allele): ...  "A2" .................  "REF" or "ALT" ........  4 or 5
-# alternate allele frequency: .............  "A1AF" ...............  "A1_FREQ" .............  7
-# effect (coefficient): ...................  "BETA" ...............  "BETA" ................ 10
-# effect standard error: ..................  "SE" .................  "SE" .................. 11
-# probability (p-value): ..................  "P" ..................  "P" ................... 13
-# count total samples: ....................  "N" ..................  "OBS_CT" ..............  9
-# probability (p-value): ..................  "Z" ..................  "NA" .................. (BETA / SE) # division by zero
-# imputation quality score: ...............  "INFO" ...............  "NA" .................. NA
-# count case samples: .....................  "NCASE" ..............  "NA" .................. NA
-# count control samples: ..................  "NCONT" ..............  "NA" .................. NA
-
-
-
 #
