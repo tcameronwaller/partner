ea9f23bf685992eafd0b51b914407e87cf5bccbf
T. Cameron Waller <tcameronwaller@gmail.com>
2022-11-17 14:50:31 -0800

    try again

--- scripts/gwas_process/format_gwas_team/translate_gwas_pgc_body_bipolar.sh
+++ scripts/gwas_process/format_gwas_team/translate_gwas_pgc_body_bipolar.sh
@@ -64,7 +64,7 @@ rm $path_file_product
 
 echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_temporary_format
 zcat $path_file_source | awk 'BEGIN { FS=","; OFS=" " } NR > 1 {
-  split($1,a,"_"); print (sub(/chr/, "", a[1]) ":" a[2]), a[1], a[2], toupper($2), toupper($3), "NA", $5, $6, $7, (3717), "NA", (1), "NA", "NA"
+  split($1,a,"_"); print (sub(/chr/, "", $a[1]) ":" $a[2]), $a[1], $a[2], toupper($2), toupper($3), "NA", $5, $6, $7, (3717), "NA", (1), "NA", "NA"
 }' >> $path_file_temporary_format
 
 # Compress file format.
