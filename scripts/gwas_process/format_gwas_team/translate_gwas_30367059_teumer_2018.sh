b12e3e893d1e5bc4c43d7b42d2a0a8f900413736
T. Cameron Waller <tcameronwaller@gmail.com>
2023-01-20 17:05:42 -0600

    begin new format translation script for GWAS summary statistics

---
+++ scripts/gwas_process/format_gwas_team/translate_gwas_30367059_teumer_2018.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# Notes
+
+# This script translates the format of GWAS summary statistics from
+# Teumer et al, Nature Communications, 2018 (PubMed:30367059).
+# Host: https://transfer.sysepi.medizin.uni-greifswald.de/thyroidomics/datasets/
+
+# Source Format
+# delimiter: comma
+# columns: MarkerName,Allele1,Allele2,Freq1,Effect,StdErr,P.value,N,I2
+
+# Format Translation
+# Identifiers of SNP variants is [chromosome]:[position]:"SNP".
+# Extract the chromosome and position.
+# Define a temporary identifier [chromosome]:[position]:"SNP".
+
+# TODO: TCW; 20 January 2023
+
+# columns: $1, [extract], extract, toupper($2), toupper($3), $4, $5, $6, $7, $8, "NA", "NA", "NA", "NA"
+
+# Product Format (Team Standard)
+# delimiter: white space
+# columns: SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT
+
+# review: TCW; ___
+
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+
+
+################################################################################
+# Organize arguments.
+
+path_file_source=${1} # full path to file for source GWAS summary statistics with GZip compression
+path_file_product=${2} # full path to file for product GWAS summary statistics in format with GZip compression
+report=${3} # whether to print reports
+
+################################################################################
+# Organize paths.
+
+name_base_file_product="$(basename $path_file_product .txt.gz)"
+path_directory_product="$(dirname $path_file_product)"
+path_directory_product_temporary="${path_directory_product}/temporary_format_${name_base_file_product}" # hopefully unique
+path_file_temporary_format="${path_directory_product_temporary}/${name_base_file_product}_format.txt"
+
+# Initialize directory.
+mkdir -p $path_directory_product
+rm -r $path_directory_product_temporary
+mkdir -p $path_directory_product_temporary
+
+# Remove any previous version of the product file.
+rm $path_file_product
+
+###########################################################################
+# Execute procedure.
+
+##########
+# Translate format of GWAS summary statistics.
+# Note that AWK interprets a single space delimiter (FS=" ") as any white space.
+
+# simple: print $1, $2, $3, toupper($4), toupper($5), $6, $10, $11, $12, $8, "NA", $7, "NA", "NA"
+
+echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_temporary_format
+zcat $path_file_source | awk 'BEGIN {FS = " "; OFS = " "} NR > 1 {
+  (a = $1); split(a, b, ":"); print b[1], $2, $3, toupper($4), toupper($5), $6, $10, $11, $12, $8, "NA", $7, "NA", "NA"
+}' >> $path_file_temporary_format
+
+# Compress file format.
+gzip -cvf $path_file_temporary_format > $path_file_product
+
+# Report.
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "----------"
+  echo "----------"
+  echo "Translate format of GWAS summary statistics."
+  echo "path to source GWAS file: " $path_file_source
+  echo "path to product GWAS file: " $path_file_product
+  echo "table after format:"
+  head -10 $path_file_temporary_format
+  echo "----------"
+  echo "----------"
+  echo "----------"
+fi
+
+# Remove temporary, intermediate files.
+rm -r $path_directory_product_temporary
+
+
+
+#
--- scripts/utility/gwas_clean/tutorial_gwas_clean.sh
+++ scripts/utility/gwas_clean/tutorial_gwas_clean.sh
@@ -28,5 +28,78 @@
 # 5. convert from GWAS catalog format to format readable by PLINK2 score function
 
 
+################################################################################
+
+
 
 ################################################################################
+# Organize paths.
+
+# Directories.
+cd ~/paths
+path_directory_tools=$(<"./waller_tools.txt")
+#path_plink2=$(<"./tools_plink2.txt")
+#path_gctb=$(<"./tools_waller_gctb.txt")
+#path_directory_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
+#path_directory_reference=$(<"./reference_tcw.txt")
+path_directory_process=$(<"./process_psychiatric_metabolism.txt")
+path_directory_dock="${path_directory_process}/dock" # parent directory for procedural reads and writes
+path_directory_product="${path_directory_dock}/test_gwas_clean"
+
+# Files.
+path_file_gwas_source="${path_directory_dock}/hormone_genetics/gwas_format_standard/32042192_ruth_2020_testosterone_female.txt.gz"
+path_file_gwas_product="${path_directory_product}/32042192_ruth_2020_testosterone_female.txt.gz"
+path_file_gwas2vcf_parameter="${path_directory_product}/importation_parameter_gwas_standard_to_gwasvcf.json"
+
+# Scripts.
+path_script_gwas_clean="${path_directory_process}/promiscuity/scripts/utility/gwas_clean/clean_gwas.sh"
+path_script_gwas_format_standard_to_gwas2vcf="${path_directory_process}/promiscuity/scripts/utility/gwas_clean/translate_gwas_standard_to_gwas2vcf.sh" # <-- do not need
+
+path_script_submit_batch="${path_directory_process}/promiscuity/scripts/utility/gctb/submit_batch_gctb_sbayesr_chromosomes.sh"
+path_script_batch_run_sbayesr="${path_directory_process}/promiscuity/scripts/utility/gctb/batch_run_gctb_sbayesr.sh"
+path_script_run_sbayesr="${path_directory_process}/promiscuity/scripts/utility/gctb/run_gctb_sbayesr.sh"
+
+# Initialize directories.
+#rm -r $path_directory_product
+mkdir -p $path_directory_product
+cd $path_directory_product
+
+
+
+###########################################################################
+# Organize parameters.
+
+
+##########
+# Installation: GWAS2VCF
+
+# https://mrcieu.github.io/gwas2vcf/tutorial/#download-gwas
+
+##########
+# Translate GWAS summary statistics from standard format to GWAS-VCF format.
+# Indexing of columns in source GWAS summary statistics bases on zero.
+
+# TODO: decompress the summary statistics
+
+echo "{
+  "chr_col": 1,
+  "pos_col": 2,
+  "snp_col": 0,
+  "ea_col": 3,
+  "oa_col": 4,
+  "beta_col": 6,
+  "se_col": 7,
+  "ncontrol_col": 9,
+  "pval_col": 8,
+  "eaf_col": 5,
+  "delimiter": "\t",
+  "header": true,
+  "build": "GRCh37"
+}" > $path_file_gwas2vcf_parameter
+
+
+
+
+
+
+#
