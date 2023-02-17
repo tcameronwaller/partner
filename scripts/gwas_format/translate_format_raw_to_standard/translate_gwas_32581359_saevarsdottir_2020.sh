672df43f5b4e5232230f1429192d6f5c70b1bf47
T. Cameron Waller <tcameronwaller@gmail.com>
2023-02-17 11:04:05 -0600

    include second awk step to remove 'chr' prefix

--- scripts/gwas_format/translate_format_raw_to_standard/translate_gwas_32581359_saevarsdottir_2020.sh
+++ scripts/gwas_format/translate_format_raw_to_standard/translate_gwas_32581359_saevarsdottir_2020.sh
@@ -83,16 +83,16 @@ echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_tempora
 # For conciseness, only support the conditions that are relevant.
 if [ "$fill_observations" == "1" ] && [ "$fill_case_control" == "1" ]; then
   zcat $path_file_source | awk -v observations=$observations -v cases=$cases -v controls=$controls 'BEGIN {FS = " "; OFS = " "} NR > 1 {
-    if ((($10 + 0) > 0) && (toupper($6) != "NA") && (toupper($7) != "NA") && (toupper($8) != "NA") && (toupper($9) != "NA"))
+    if ((toupper($10) != "NA") && (($10 + 0) > 0) && (toupper($6) != "NA") && (toupper($7) != "NA") && (toupper($8) != "NA") && (toupper($9) != "NA"))
       # Calculate frequency and imputation quality score from non-missing values.
       print $3, $1, $2, toupper($5), toupper($4), (($6*0.459)+($8*0.541)), log($10), "NA", $11, (observations), "NA", (($7*0.459)+($9*0.541)), (cases), (controls)
-    else if ((($10 + 0) > 0) && (toupper($6) != "NA") && (toupper($7) != "NA") && (toupper($8) == "NA") && (toupper($9) == "NA"))
+    else if ((toupper($10) != "NA") && (($10 + 0) > 0) && (toupper($6) != "NA") && (toupper($7) != "NA") && (toupper($8) == "NA") && (toupper($9) == "NA"))
       # Report non-missing values from Iceland Biobank.
       print $3, $1, $2, toupper($5), toupper($4), ($6), log($10), "NA", $11, (observations), "NA", ($7), (cases), (controls)
-    else if ((($10 + 0) > 0) && (toupper($6) == "NA") && (toupper($7) == "NA") && (toupper($8) != "NA") && (toupper($9) != "NA"))
+    else if ((toupper($10) != "NA") && (($10 + 0) > 0) && (toupper($6) == "NA") && (toupper($7) == "NA") && (toupper($8) != "NA") && (toupper($9) != "NA"))
       # Report non-missing values from UK Biobank.
       print $3, $1, $2, toupper($5), toupper($4), ($8), log($10), "NA", $11, (observations), "NA", ($9), (cases), (controls)
-    else if ((($10 + 0) > 0))
+    else if ((toupper($10) != "NA") && (($10 + 0) > 0))
       # Report missing values for allele frequency and a meaningless imputation quality score.
       print $3, $1, $2, toupper($5), toupper($4), "NA", log($10), "NA", $11, (observations), "NA", (1.0), (cases), (controls)
     else
@@ -100,6 +100,10 @@ if [ "$fill_observations" == "1" ] && [ "$fill_case_control" == "1" ]; then
       print $3, $1, $2, toupper($5), toupper($4), "NA", "NA", "NA", $11, (observations), "NA", (1.0), (cases), (controls)
   }' >> $path_file_temporary_format
 fi
+# Remove "chr" prefix from chromosome identifiers.
+awk 'BEGIN {FS = " "; OFS = " "} NR > 1 { sub(/chr/,"", $2); print } ' $path_file_temporary_format > $path_file_temporary_format
+#awk 'BEGIN {FS = " "; OFS = " "} NR > 1 { gsub(/chr/,"", $2); print } ' $path_file_temporary_format > $path_file_temporary_format
+
 
 # Compress file format.
 gzip -cvf $path_file_temporary_format > $path_file_product
