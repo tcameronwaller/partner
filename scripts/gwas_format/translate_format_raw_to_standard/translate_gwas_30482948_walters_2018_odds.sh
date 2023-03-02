332423e050dde12e9357ea20ffb52032824362cc
T. Cameron Waller <tcameronwaller@gmail.com>
2023-03-01 18:21:40 -0600

    new format translation scripts

---
+++ scripts/gwas_format/translate_format_raw_to_standard/translate_gwas_30482948_walters_2018_odds.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# Notes
+
+# This script translates the format of GWAS summary statistics from
+# Walters et al, Nature Neuroscience, 2018
+# (PubMed:30482948).
+# Host: https://pgc.unc.edu/
+# Host: https://figshare.com/articles/dataset/sud2018-alc/14672187
+
+# Source Format
+# Human Genome Assembly: GRCh37 (hg19; UK Biobank)
+# Effect allele: "A1"
+# Delimiter: white space
+# Columns: CHR SNP BP A1 A2 INFO OR SE P   Neff
+#          1   2   3  4  5  6    7  8  9   10   (TCW; 1 March 2023)
+# Note: It seems that the authors already converted the standard error of the odds ratio to the standard error for a beta coefficient.
+
+# Product Format (Team Standard)
+# effect allele: "A1"
+# delimiter: white space
+# columns: SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT
+
+# Review: TCW; __ March 2023
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+
+
+################################################################################
+# Organize arguments.
+
+path_file_source=${1} # full path to file for source GWAS summary statistics with GZip compression
+path_file_product=${2} # full path to file for product GWAS summary statistics in format with GZip compression
+fill_observations=${3} # logical binary indicator of whether to fill count of observations across all variants
+observations=${4} # count of observations
+fill_case_control=${5} # logical binary indicator of whether to fill counts of cases and controls across all variants
+cases=${6} # count of cases
+controls=${7} # count of controls
+report=${8} # whether to print reports
+
+################################################################################
+# Organize paths.
+
+name_base_file_product="$(basename $path_file_product .txt.gz)"
+path_directory_product="$(dirname $path_file_product)"
+path_directory_product_temporary="${path_directory_product}/temporary_format_${name_base_file_product}" # hopefully unique
+path_file_temporary_format="${path_directory_product_temporary}/${name_base_file_product}_format.txt"
+
+# Initialize directory.
+mkdir -p $path_directory_product
+rm -r $path_directory_product_temporary
+mkdir -p $path_directory_product_temporary
+
+# Remove any previous version of the product file.
+rm $path_file_product
+
+###########################################################################
+# Execute procedure.
+
+# Note: TCW; 17 February 2023
+# The logarithm of a negative number or zero is undefined.
+
+##########
+# Translate format of GWAS summary statistics.
+# Note that AWK interprets a single space delimiter (FS=" ") as any white space.
+echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_temporary_format
+# For conciseness, only support the conditions that are relevant.
+if [ "$fill_observations" != "1" ] && [ "$fill_case_control" == "1" ]; then
+  zcat $path_file_source | awk -v cases=$cases -v controls=$controls 'BEGIN {FS = " "; OFS = " "} NR > 1 {
+    if ((toupper($7) != "NA") && (($7 + 0) > 0))
+      print $2, $1, $3, toupper($4), toupper($5), "NA", log($7), $8, $9, int($10), "NA", $6, (cases), (controls)
+    else
+      print $2, $1, $3, toupper($4), toupper($5), "NA", "NA", $8, $9, int($10), "NA", $6, (cases), (controls)
+  }' >> $path_file_temporary_format
+fi
+
+# Compress file format.
+gzip -cvf $path_file_temporary_format > $path_file_product
+
+# Report.
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "----------"
+  echo "----------"
+  echo "Translate format of GWAS summary statistics."
+  echo "path to source GWAS file: " $path_file_source
+  echo "path to product GWAS file: " $path_file_product
+  echo "table after format:"
+  head -10 $path_file_temporary_format
+  echo "----------"
+  echo "----------"
+  echo "----------"
+fi
+
+# Remove temporary, intermediate files.
+rm -r $path_directory_product_temporary
+
+
+
+#
---
+++ scripts/gwas_format/translate_format_raw_to_standard/translate_gwas_30643251_liu_2019.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# Notes
+
+# This script translates the format of GWAS summary statistics from
+# Liu et al, Nature Genetics, 2019
+# (PubMed:30643251).
+# Host: https://genome.psych.umn.edu/index.php/GSCAN
+# Host: https://conservancy.umn.edu/handle/11299/201564
+
+# Source Format
+# Human Genome Assembly: GRCh37 (hg19; UK Biobank)
+# Effect allele: "ALT" ("Beta based on the alternate allele")
+# Delimiter: white space
+# Columns: CHROM POS RSID REF ALT AF STAT PVALUE BETA SE   N   EFFECTIVE_N Number_of_Studies ANNO ANNOFULL
+#          1     2   3    4   5   6  7    8      9    10   11  12          13                14   15       (TCW; 1 March 2023)
+
+# Product Format (Team Standard)
+# effect allele: "A1"
+# delimiter: white space
+# columns: SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT
+
+# Review: TCW; __ March 2023
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+
+
+################################################################################
+# Organize arguments.
+
+path_file_source=${1} # full path to file for source GWAS summary statistics with GZip compression
+path_file_product=${2} # full path to file for product GWAS summary statistics in format with GZip compression
+fill_observations=${3} # logical binary indicator of whether to fill count of observations across all variants
+observations=${4} # count of observations
+fill_case_control=${5} # logical binary indicator of whether to fill counts of cases and controls across all variants
+cases=${6} # count of cases
+controls=${7} # count of controls
+report=${8} # whether to print reports
+
+################################################################################
+# Organize paths.
+
+name_base_file_product="$(basename $path_file_product .txt.gz)"
+path_directory_product="$(dirname $path_file_product)"
+path_directory_product_temporary="${path_directory_product}/temporary_format_${name_base_file_product}" # hopefully unique
+path_file_temporary_format="${path_directory_product_temporary}/${name_base_file_product}_format.txt"
+
+# Initialize directory.
+mkdir -p $path_directory_product
+rm -r $path_directory_product_temporary
+mkdir -p $path_directory_product_temporary
+
+# Remove any previous version of the product file.
+rm $path_file_product
+
+###########################################################################
+# Execute procedure.
+
+# Note: TCW; 17 February 2023
+# The logarithm of a negative number or zero is undefined.
+
+##########
+# Translate format of GWAS summary statistics.
+# Note that AWK interprets a single space delimiter (FS=" ") as any white space.
+echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_temporary_format
+# For conciseness, only support the conditions that are relevant.
+if [ "$fill_observations" != "1" ] && [ "$fill_case_control" != "1" ]; then
+  zcat $path_file_source | awk 'BEGIN {FS = " "; OFS = " "} NR > 1 {
+    print $3, $1, $2, toupper($5), toupper($4), $6, $9, $10, $8, $11, "NA", (1.0), "NA", "NA"
+  }' >> $path_file_temporary_format
+fi
+
+# Compress file format.
+gzip -cvf $path_file_temporary_format > $path_file_product
+
+# Report.
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "----------"
+  echo "----------"
+  echo "Translate format of GWAS summary statistics."
+  echo "path to source GWAS file: " $path_file_source
+  echo "path to product GWAS file: " $path_file_product
+  echo "table after format:"
+  head -10 $path_file_temporary_format
+  echo "----------"
+  echo "----------"
+  echo "----------"
+fi
+
+# Remove temporary, intermediate files.
+rm -r $path_directory_product_temporary
+
+
+
+#
