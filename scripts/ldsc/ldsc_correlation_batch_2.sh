ee24eea40ca4e05d91f31243e2cef6f361f1f9bb
T. Cameron Waller <tcameronwaller@gmail.com>
2023-09-05 14:28:41 -0400

    change parameter name

--- scripts/ldsc/ldsc_correlation_batch_2.sh
+++ scripts/ldsc/ldsc_correlation_batch_2.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=tcw_example               # name of job
 #SBATCH --mail-user=waller.tcameron@mayo.edu # email address
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_50   # situations in which to send email
-#SBATCH --partition=cpu-short                  # queue: cpu-short, cpu-med, cpu-long
+#SBATCH --partition=cpu-short                # queue: cpu-short, cpu-med, cpu-long
 #SBATCH --nodes=1                            # count of cluster nodes (CPUs)
-#SBATCH --ntasks=4                            # count of cores or threads on node
+#SBATCH --tasks-per-node=4                   # count of cores or threads on node
 #SBATCH --time=0-00:30:00                    # time allocation request (days-hours:minutes:seconds)
 #SBATCH --mem=4G                             # memory per node (per CPU)
 #SBATCH --output logs/%x.%A.%N.%j.%a.stdout
