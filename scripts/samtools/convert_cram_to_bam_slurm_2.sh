7a2f91a2c786d536475b186873a9dc8a9c2a6b41
T. Cameron Waller <tcameronwaller@gmail.com>
2024-07-10 15:14:30 -0500

    test sort after converstion from CRAM to BAM format

--- scripts/samtools/convert_cram_to_bam_slurm_2.sh
+++ scripts/samtools/convert_cram_to_bam_slurm_2.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=cpu-short                # queue: cpu-short, cpu-med, cpu-long
 #SBATCH --nodes=1                            # count of cluster nodes (CPUs)
 #SBATCH --ntasks-per-node=2                  # count of CPU cores or threads on node
-#SBATCH --mem=4G                             # memory per node (per CPU)
+#SBATCH --mem=3G                             # memory per node (per CPU)
 #SBATCH --time=0-01:00:00                    # time allocation request (days-hours:minutes:seconds)
 #SBATCH --output ./%x.%A.%N.%j.%a.stdout
 #SBATCH --error ./%x.%A.%N.%j.%a.stderr
@@ -27,7 +27,7 @@
 
 # Conversion of file from CRAM format to BAM format only requires about 200
 # Megabytes of memory per node.
-# Sort of file in BAM format requires 2-4 Gigabytes of memory per node.
+# Sort of file in BAM format requires about 2 Gigabytes of memory per node.
 
 # Check status in queue.
 # squeue -u {user}
