9302668b3daedacad4e90ba2524fd8bb766ee9de
T. Cameron Waller <tcameronwaller@gmail.com>
2024-07-10 16:56:46 -0500

    implement Slurm management scripts for batch job of merge BAM files

---
+++ scripts/samtools/merge_bam_files_slurm_1.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+#chmod -R 777
+
+###############################################################################
+# Author: T. Cameron Waller
+# Date, first execution: 10 July 2024
+# Date, last execution or modification: 10 July 2024
+# Review: TCW; 10 July 2024
+###############################################################################
+# Note
+
+
+###############################################################################
+# Organize arguments.
+
+path_file_parallel_instances=${1} # full path to file in text format with list of information for each instance in parallel batch
+path_directory_parallel=${2} # full path to directory for files relating to management of parallel batch
+threads=${3} # count of concurrent or parallel process threads on node cores
+report=${4} # whether to print reports to terminal
+path_script_parallel_2=${5} # full path to file of script for execution of job procedure
+path_script_parallel_3=${6} # full path to file of script for execution of job procedure
+path_execution_samtools=${7} # full path to executable file for SamTools
+
+###############################################################################
+# Organize paths.
+
+# Initialize directories.
+cd $path_directory_parallel # execute parallel batch procedures from within this directory
+
+###############################################################################
+# Organize job instances in parallel batch.
+
+# Read job instances in parallel batch.
+readarray -t instances_parallel < $path_file_parallel_instances
+count_parallel_instances=${#instances_parallel[@]}
+index_array_maximum=$(($count_parallel_instances - 1)) # indices start at zero in array of parallel batch job instances
+
+###############################################################################
+# Report.
+
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "script:"
+  echo $0 # Print full file path to script.
+  echo "merge_bam_files_slurm_1.sh"
+  echo "----------"
+  echo "count of instances in parallel batch: " $count_parallel_instances
+  echo "maximum index in array of instances: " $index_array_maximum
+  echo "----------"
+  echo "first instance: " ${instances_parallel[0]} # notice base-zero indexing
+  echo "last instance: " ${instances_parallel[$index_array_maximum]}
+  echo "----------"
+fi
+
+###############################################################################
+# Submit batch of parallel job instances to Slurm Scheduler to manage
+# processes on computational grid cluster.
+# Indices start at zero in array of parallel batch job instances.
+
+# Determine shift in indices if necessary.
+# NCSA mForge implementation of SLURM has value of "MaxArraySize" of 10,000
+# as confirmed by TCW on 14 May 2024.
+# Find the value of "MaxArraySize" within the text file at path
+# "/etc/slurm/slurm.conf".
+# SLURM will not allow an array index greater than "MaxArraySize".
+# It is necessary to split larger jobs.
+index_shift=0
+
+if true; then
+  sbatch \
+  --exclude=mforge160 \
+  --array 0-${index_array_maximum}:1 \
+  --chdir $path_directory_parallel \
+  $path_script_parallel_2 \
+  $path_file_parallel_instances \
+  $count_parallel_instances \
+  $path_directory_parallel \
+  $index_shift \
+  $threads \
+  $report \
+  $path_script_parallel_3 \
+  $path_execution_samtools
+fi
+
+
+###############################################################################
+# End.
---
+++ scripts/samtools/merge_bam_files_slurm_2.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+#SBATCH --job-name=samtools                  # name of job
+#SBATCH --mail-user=waller.tcameron@mayo.edu # email address
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_50   # situations in which to send email
+#SBATCH --partition=cpu-short                # queue: cpu-short, cpu-med, cpu-long
+#SBATCH --nodes=1                            # count of cluster nodes (CPUs)
+#SBATCH --ntasks-per-node=2                  # count of CPU cores or threads on node
+#SBATCH --mem=5G                             # memory per node (per CPU)
+#SBATCH --time=0-01:00:00                    # time allocation request (days-hours:minutes:seconds)
+#SBATCH --output ./%x.%A.%N.%j.%a.stdout
+#SBATCH --error ./%x.%A.%N.%j.%a.stderr
+#SBATCH --signal=USR1@60
+
+###############################################################################
+# Note.
+
+# This script must execute within a working directory set up as the parent
+# directory of child directories and files for management of parallel batch.
+
+# NCSA mForge implementation of SLURM has value of "MaxArraySize" of 10,000
+# as confirmed by TCW on 14 May 2024.
+# Find the value of "MaxArraySize" within the text file at path
+# "/etc/slurm/slurm.conf".
+# SLURM will not allow an array index greater than "MaxArraySize".
+# It is necessary to split larger jobs.
+
+# Check status in queue.
+# squeue -u {user}
+
+###############################################################################
+# Organize arguments.
+
+path_file_parallel_instances=${1} # full path to file in text format with list of information for each instance in parallel batch
+count_parallel_instances=${2} # count of instances in parallel batch
+path_directory_parallel=${3} # full path to directory for files relating to management of parallel batch
+index_shift=${4} # shift index necessary when count of instances exceeds system variable "MaxArraySize"
+threads=${5} # count of concurrent or parallel process threads on node cores
+report=${6} # whether to print reports to terminal
+path_script_parallel_3=${7} # full path to file of script for execution of job procedure
+path_execution_samtools=${8} # full path to executable file for SamTools
+
+###############################################################################
+# Organize parameters.
+
+# Determine current job instance of parallel batch.
+index_parallel=$((SLURM_ARRAY_TASK_ID + index_shift)) # indices start at zero in array of parallel batch job instances
+readarray -t instances_parallel < $path_file_parallel_instances
+instance=${instances_parallel[$index_parallel]}
+
+# Separate fields from instance.
+IFS=";" read -r -a array <<< "${instance}"
+identifier_sample="${array[0]}"
+path_file_source="${array[1]}"
+path_file_product="${array[2]}"
+
+###############################################################################
+# Report.
+
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "script:"
+  echo $0 # Print full file path to script.
+  echo "merge_bam_files_slurm_2.sh"
+  echo "----------"
+  echo "instance in parallel batch"
+  echo "Slurm job id: " $SLURM_JOB_ID
+  echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+  echo "----------"
+  echo "path to source file:"
+  echo $path_file_source
+  echo "path to product file:"
+  echo $path_file_product
+  echo "----------"
+fi
+
+
+###############################################################################
+# Execute procedure.
+
+# Execute main procedure.
+if true; then
+  /usr/bin/bash $path_script_parallel_3 \
+  $path_file_source \
+  $path_file_product \
+  $threads \
+  $report \
+  $path_execution_samtools
+fi
+
+###############################################################################
+# End.
