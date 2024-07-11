eb26d918c9cc5f6ec918ab47c4e299b2269fb454
T. Cameron Waller <tcameronwaller@gmail.com>
2024-07-11 17:13:16 -0500

    Slurm management of parallel batch for sort and index of BAM files

--- scripts/samtools/sort_index_bam.sh
+++ scripts/samtools/sort_index_bam.sh
@@ -5,23 +5,17 @@
 
 ###############################################################################
 # Author: T. Cameron Waller
-# Date, first execution: 10 July 2024
-# Date, last execution or modification: 10 July 2024
-# Review: TCW; 10 July 2024
+# Date, first execution: 11 July 2024
+# Date, last execution or modification: 11 July 2024
+# Review: TCW; 11 July 2024
 ###############################################################################
 # Note
 
-# The file in "path_file_source" is in text format and gives a list of paths to
-# the multiple source files in BAM format to merge together.
-
-# SamTools also has a "concat" method or a similar name that combines files in
-# BAM format without regard for sort sequence of coordinates. This method is
-# more efficient if there will be a sort after the combination.
 
 ###############################################################################
 # Organize arguments.
 
-path_file_source=${1} # full path to file in text format with list of paths to files in BAM format for merge
+path_file_source=${1} # full path to file for source genomic or transcriptomic sequence information in CRAM format
 path_file_product=${2} # full path to file for product genomic or transcriptomic sequence information in BAM format
 threads=${3} # count of concurrent or parallel process threads on node cores
 report=${4} # whether to print reports to terminal
@@ -30,20 +24,17 @@ path_execution_samtools=${5} # full path to executable file for SamTools
 ###############################################################################
 # Organize paths.
 
-stamp_date=$(date +%Y-%m-%d)
+#stamp_date=$(date +%Y-%m-%d)
 name_base_file_product="$(basename $path_file_product .bam)"
 path_directory_product="$(dirname $path_file_product)"
-#path_directory_temporary="${path_directory_product}/temporary_${name_base_file_product}_${stamp_date}" # hopefully unique
-#path_file_temporary_1="${path_directory_temporary}/${name_base_file_product}_temporary_1.bam"
+path_file_product_index="${path_directory_product}/${name_base_file_product}.bam.bai"
 
 # Initialize directory.
 mkdir -p $path_directory_product
-#rm -r $path_directory_temporary
-#mkdir -p $path_directory_temporary
 
 # Remove any previous version of the product file.
-#rm $path_file_temporary_1
 rm $path_file_product
+rm $path_file_product_index
 
 ###############################################################################
 # Organize parameters.
@@ -53,12 +44,19 @@ rm $path_file_product
 # Execute procedure.
 
 if true; then
-  # Convert file from CRAM format to BAM format.
+  # Sort coordinates of file in BAM format.
   $path_execution_samtools \
-  merge \
-  --threads $threads \
-  -b $path_file_source \
-  -o $path_file_product
+  sort \
+  -@ $threads \
+  -o $path_file_product \
+  $path_file_source
+  # Create index for file in BAM format.
+  $path_execution_samtools \
+  index \
+  --bai \
+  -threads $threads \
+  -o $path_file_product_index \
+  $path_file_product
 fi
 
 
@@ -71,16 +69,13 @@ if [ "$report" == "true" ]; then
   echo "----------"
   echo "Script:"
   echo $0 # Print full file path to script.
-  echo "Merge together multiple files in BAM format."
+  echo "Sort coordinates and create index for file in BAM format."
   echo "----------"
   echo "path to source file: " $path_file_source
-  echo "path to product file: " $path_file_product
+  echo "path to product sort file: " $path_file_product
+  echo "path to product index file: " $path_file_product_index
   echo "----------"
 fi
-
-##########
-# Remove temporary, intermediate files.
-#rm -r $path_directory_temporary
 
 ###############################################################################
 # End.
