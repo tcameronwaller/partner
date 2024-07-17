f780b6aa77bf9de9410dc634310dfb1d93440f72
T. Cameron Waller <tcameronwaller@gmail.com>
2024-07-16 20:18:32 -0500

    generate novel identifiers for anonymity

--- scripts/htseq/read_extract_table_column_sample_identifiers.sh
+++ scripts/htseq/read_extract_table_column_sample_identifiers.sh
@@ -52,23 +52,42 @@ rm $path_file_table_product
 head -1 $path_file_table_source > $path_file_temporary_1
 
 # Replace problematic delimiters.
-sed -i 's%\\t%;%g' $path_file_temporary_1
+#sed -i 's%\\t%;%g' $path_file_temporary_1
 
 # Collect lines for product table.
 lines_product=()
 line="identifier_original;identifier_novel;identifier_original_simple"
 lines_product+=($line)
+# Collect unique novel identifiers.
+declare -A identifiers_novel_unique # initialize an associative array
 # Read first line of header names for columns in table.
-IFS=$';' read -r -a array_line < $path_file_temporary_1
+IFS=$'\t' read -r -a array_line < $path_file_temporary_1
 # Iterate across header names of columns in table.
 for name_full in "${array_line[@]}"; do
+  # Initialize and reset the novel identifier.
+  identifier_novel=""
+  # Extract keys of associative array the unique original header names.
+  string_identifiers_check=${!identifiers_novel_unique[@]} # transfers a space-delimited list of keys
   # Extract base name of file.
   name_base="$(basename $name_full .bam)"
   # Generate novel identifier.
-  identifier_novel=`cat /dev/urandom | tr -dc [:alpha:][:digit:] | head -c10`
-  # Define line.
-  line="${name_full};${identifier_nove};${name_base}"
-  lines_product+=($line)
+  identifier_novel_prefix=`cat /dev/urandom | tr -dc [:upper:] | head -c3`
+  identifier_novel_suffix=`cat /dev/urandom | tr -dc [:digit:] | head -c4`
+  identifier_novel="${identifier_novel_prefix}-${identifier_novel_suffix}"
+  # Make sure that the novel identifier does not already exist in the collection.
+  if [[ "${string_identifiers_check}" =~ "${identifier_novel}" ]]; then
+    # Generate another novel identifier.
+    # The probability of generating an existing identifier twice must be small.
+    identifier_novel_prefix=`cat /dev/urandom | tr -dc [:upper:] | head -c3`
+    identifier_novel_suffix=`cat /dev/urandom | tr -dc [:digit:] | head -c4`
+    identifier_novel="${identifier_novel_prefix}-${identifier_novel_suffix}"
+  fi
+  if [[ ! "${string_identifiers_check}" =~ "${identifier_novel}" ]]; then
+    identifiers_novel_unique[$identifier_novel]=0 # assign a key-value pair
+    # Define line.
+    line="${name_full};${identifier_novel};${name_base}"
+    lines_product+=($line)
+  fi
 done
 
 # Write to file lines for table.
