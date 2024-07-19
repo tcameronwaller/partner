8190488ed51d93b01310c2ef08d7d648ae654c80
T. Cameron Waller <tcameronwaller@gmail.com>
2024-07-19 15:00:49 -0500

    make more generalizable and versatile the restoration script for local execution of code from main projects

--- scripts/restore_repository_local.sh
+++ scripts/restore_repository_local.sh
@@ -6,8 +6,8 @@
 ###############################################################################
 # Author: T. Cameron Waller
 # Date, first execution: 17 July 2024
-# Date, last execution or modification: 17 July 2024
-# Review: TCW; 17 July 2024
+# Date, last execution or modification: 19 July 2024
+# Review: TCW; 19 July 2024
 ###############################################################################
 # Note
 
